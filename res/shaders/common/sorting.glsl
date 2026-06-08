0541c5fa21356b450b7ce2d705b250690babc46b
nathanrun1 <nathanmreilly@gmail.com>
2026-06-08 12:35:53 -0700

    wip: key sort now adapted to 64 bit (uvec2) keys

--- res/shaders/common/sorting.glsl
+++ res/shaders/common/sorting.glsl
@@ -1,3 +1,5 @@
+#define BITS_PER_UINT 32
+
 uint histogram_index(uint block_id, uint digit, uint num_blocks) {
     return digit * num_blocks + block_id;
 }
@@ -6,7 +8,11 @@ uint key_index(uint block_id, uint offset, uint block_size) {
     return block_id * block_size + offset;
 }
 
-uint digit_value(uint key, uint digit_size, uint digit_id) {
-    uint mask = ((1 << digit_size) - 1) << (digit_id * digit_size);
-    return (key & mask) >> (digit_id * digit_size);
+/** Retrieves digit value of a given key, assumes digit size <= uint size and uint size % digit size = 0 */
+uint digit_value(uvec2 key, uint digit_size, uint digit_id) {
+    uint bit_offset = digit_id * digit_size;
+    uint word = bit_offset >= BITS_PER_UINT ? key.x : key.y;
+    uint local_shift = bit_offset % BITS_PER_UINT;
+    uint mask = (1u << digit_size) - 1u;
+    return (word >> local_shift) & mask;
 }
